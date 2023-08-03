e592279dd28dc2498d8782211e8f5e6ffcd92a5d
Erick Brandon <eflores@tiprotec.com>
2023-08-02 20:06:53 -0600

    corrección

--- resources/views/Dashboard/layout/footer.blade.php
+++ resources/views/Dashboard/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <script src="{{asset('assets/js/vendor-all.min.js')}}"></script>
 <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
-{{-- <script src="{{asset('assets/js/pcoded.min.js')}}"></script> --}}
+<script src="{{asset('assets/js/pcoded.min.js')}}"></script>
 <script src="{{asset('assets/plugins/sweetalert/js/sweetalert.min.js')}}"></script>
 <script src="{{asset('js-farmacia/logout.js')}}"></script>
 @yield('extras_footer')
