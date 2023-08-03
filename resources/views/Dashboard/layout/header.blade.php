32a464d091004423ddf697e7366e3f3d6cc3a872
Erick Brandon <eflores@tiprotec.com>
2023-08-02 19:29:19 -0600

    Incersión del icon en el titulo

--- resources/views/Dashboard/layout/header.blade.php
+++ resources/views/Dashboard/layout/header.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <link rel="stylesheet" href="{{asset('assets/fonts/fontawesome/css/fontawesome-all.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-    
+    <link rel="shortcut icon" href="{{asset('logo/FP.ico')}}">
      @yield('extras_header')
 </head>
 <body class="layout-6">
