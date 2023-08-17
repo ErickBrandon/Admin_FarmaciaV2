29cacb8730db497cc313c78bc0a6701b005a0cbc
Erick Brandon <eflores@tiprotec.com>
2023-08-17 17:59:33 -0600

    versionamiento en css y js 3

--- resources/views/Dashboard/layout/header.blade.php
+++ resources/views/Dashboard/layout/header.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    {{$verssion = now()->day}}
     <title>FarmaPlus</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <link rel="stylesheet" href="{{asset('assets/fonts/fontawesome/css/fontawesome-all.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}?v={{now()->day}}">
     <link rel="shortcut icon" href="{{asset('logo/FP.ico')}}">
      @yield('extras_header')
 </head>
