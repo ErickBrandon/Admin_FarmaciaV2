33bc05fdf6887cd06598460dbf98b4e705965695
Erick Brandon <eflores@tiprotec.com>
2023-08-16 21:27:49 -0600

    orden en migracion productos

--- database/migrations/2023_01_03_010401_create_productos_table.php
+++ database/migrations/2023_01_03_010401_create_productos_table.php
@@ -15,15 +15,16 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->String('Codigo',25);
+            $table->String('Codigo',50);
             $table->string('Producto',250);
-            $table->float('Precio');
             $table->integer('Existencias');
-            $table->string('TipoVenta',8);
-            $table->date('Caducidad');
+            $table->float('Precio');
             $table->float('Costo');
+            $table->string('TipoVenta',10);
+            $table->date('Caducidad')->nullable();
+            
             $table->unsignedBigInteger('farmacia_id')->nullable();
-            $table->date('Ultima_asignacion');
+            $table->date('Ultima_asignacion')->nullable();
 
             
             $table->foreign('farmacia_id')
